047121f5530e59618239b88cb6d955e15c124ec3
baton <vladzorkin2002@gmail.com>
2025-06-24 18:53:34 +0900

    Наглядно сделал вывод 12 месяцев

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@ import Calendar from "./components/Calendar.vue";
 </script>
 <template>
   <div class="wrapper">
-    <Calendar></Calendar>
+    <Calendar v-for="month in 12">
+      {{ month }}
+    </Calendar>
   </div>
 </template>
 <style scoped>
 .wrapper {
-  height: 80vh;
+  flex-wrap: wrap;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
